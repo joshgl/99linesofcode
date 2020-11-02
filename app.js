c13a4a524c9d8dd303b7da4ea31073761f483be4
Garlick <joshua.m.garlick@gmail.com>
2020-11-01 19:18:19 -0500

    first draft

---
+++ app.js
@@ -0,0 +1,31 @@
+let friends = ["Bobby", "Jacob", "Trish", "Mike", "Morgan"];
+
+
+for (name in friends){
+    for (let j = 99; j != 0 ; j--){
+        if (j > 1) {
+           console.log(j + " lines of code in the file, " + j + "lines of code; " + name + "strikes one out, clears it all out, " + j - 1 + "lines of code in the file");
+        } else {
+          console.log("1 line of code in the file, 1 line of code; " + name + "strikes one out, clears it all out, 0 lines of code in the file");
+
+        };
+         
+     };
+
+    };
+
+
+    let friends = ["Bobby", "Jacob", "Trish", "Mike", "Morgan"];
+
+    friends.forEach(function (name){
+        for (var j = 99; j > 0 ; j--){
+            if (j > 1) {
+                console.log( j + " lines of code in the file, " + j + " lines of code; " + name + " strikes one out, clears it all out, " + (j - 1) +  " lines of code in the file");
+             } else {
+               console.log("1 line of code in the file, 1 line of code; " + name + " strikes one out, clears it all out, 0 lines of code in the file");
+     
+             }
+              
+          }
+
+    });
